bb87b6aeb6de7c4f312d6c272b49a0295de1cb87
Divyanshu Gupta <44648798+divyanshuguptaososofficial@users.noreply.github.com>
2020-11-12 18:17:56 +0530

    Card  Screen Added

--- src/pages/HomeScreen.js
+++ src/pages/HomeScreen.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { View, Text, Button, StyleSheet, StatusBar ,TouchableOpacity} from 'react-native';
+import { View, Text, Button, StyleSheet, StatusBar ,TouchableOpacity,Image, ImageBackground} from 'react-native';
 import { useTheme } from '@react-navigation/native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import {dummyData} from './DummyData'
 import Carousel from '../components/Carousel';
+import {TextInput,ScrollView} from 'react-native-gesture-handler';
 const HomeScreen = ({navigation}) => {
 
   const { colors } = useTheme();
@@ -11,6 +12,11 @@ const HomeScreen = ({navigation}) => {
   const theme = useTheme();
   
     return (
+      <ScrollView 
+      vertical
+      showsVerticalScrollIndicator={true}
+      style={{height:400}}
+  >
       <View style={styles.container}>
         <View style={styles.categoryContainer}>
         <TouchableOpacity
@@ -61,6 +67,313 @@ const HomeScreen = ({navigation}) => {
       </View>
      
       <Carousel data = {dummyData}></Carousel>
+
+      <View style={{
+                   flexDirection:"row",
+                   paddingHorizontal:20,
+                   width:"100%",
+                   alignItems:"center"
+               }}>
+                   <View style={{width:"50%"}}>
+                        <Text style={{
+                            fontWeight:"bold",
+                            fontSize:17,
+                            color:"#000"
+                        }}>Government Schemes</Text>
+                       
+
+                   </View>
+                   <View style={{width:"50%", alignItems:"flex-end"}}>
+                        <View style={{
+                            backgroundColor:"#000",
+                            paddingHorizontal:20,
+                            paddingVertical:5,
+                            borderRadius:15
+                        }}>
+                            <Text style={{
+                                fontWeight:"bold",
+                                fontSize:13,
+                                color:"#FFF"
+                            }}>View All</Text>
+                        </View>
+                   </View>
+               </View>
+
+
+               <ScrollView 
+                    horizontal
+                    showsHorizontalScrollIndicator={false}
+                    style={{height:400}}
+                >
+                    
+                    <TouchableOpacity 
+                        onPress={()=>navigation.navigate("Detail")}
+                        style={{
+                            height:250,
+                            elevation:2,
+                            backgroundColor:"#000",
+                            marginLeft:20,
+                            marginTop:20,
+                            borderRadius:15,
+                            marginBottom:10,
+                            width:160
+                        }}
+                    >
+                       
+                        <View style={{
+                            flexDirection:"row",
+                            paddingTop:10,
+                            paddingHorizontal:10,
+                            alignItems:"center"
+                        }}>
+                            <Text style={{
+                                fontWeight:"bold",
+                                fontSize: 25,
+                                color:"#fff"
+                            }}>The Rashtriya Kishor Swasthya Karyakram</Text>
+                           
+                        </View>
+                        <Text style={{
+                            paddingHorizontal:10,
+                            fontWeight:"bold",
+                            color:"#fff",
+                            paddingTop:35,
+                            alignSelf :'center'
+                        }}>
+                            APPLY NOW
+                        </Text>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity 
+                        onPress={()=>navigation.navigate("Detail")}
+                        style={{
+                            height:250,
+                            elevation:2,
+                            backgroundColor:"#FFF",
+                            marginLeft:20,
+                            marginTop:20,
+                            borderRadius:15,
+                            marginBottom:10,
+                            width:160
+                        }}
+                    >
+                       
+                        <View style={{
+                            flexDirection:"row",
+                            paddingTop:10,
+                            paddingHorizontal:10,
+                            alignItems:"center"
+                        }}>
+                            <Text style={{
+                                fontWeight:"bold",
+                                fontSize: 25
+                            }}>The Rashtriya Kishor Swasthya Karyakram</Text>
+                           
+                        </View>
+                        <Text style={{
+                            paddingHorizontal:10,
+                            fontWeight:"bold",
+                            color:"#b1e5d3",
+                            paddingTop:35,
+                            alignSelf :'center'
+                        }}>
+                            APPLY NOW
+                        </Text>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity 
+                        onPress={()=>navigation.navigate("Detail")}
+                        style={{
+                            height:250,
+                            elevation:2,
+                            backgroundColor:"#000",
+                            marginLeft:20,
+                            marginTop:20,
+                            borderRadius:15,
+                            marginBottom:10,
+                            width:160
+                        }}
+                    >
+                       
+                        <View style={{
+                            flexDirection:"row",
+                            paddingTop:10,
+                            paddingHorizontal:10,
+                            alignItems:"center"
+                        }}>
+                            <Text style={{
+                                fontWeight:"bold",
+                                fontSize: 25,
+                                color:"#fff"
+                            }}>The Rashtriya Kishor Swasthya Karyakram</Text>
+                           
+                        </View>
+                        <Text style={{
+                            paddingHorizontal:10,
+                            fontWeight:"bold",
+                            color:"#fff",
+                            paddingTop:35,
+                            alignSelf :'center'
+                        }}>
+                            APPLY NOW
+                        </Text>
+                    </TouchableOpacity>
+
+
+                </ScrollView>
+
+                <View style={{
+                  paddingTop:5,
+                   flexDirection:"row",
+                   paddingHorizontal:20,
+                   width:"100%",
+                   alignItems:"center"
+               }}>
+                   <View style={{width:"50%"}}>
+                        <Text style={{
+                            fontWeight:"bold",
+                            fontSize:17,
+                            color:"#000"
+                        }}>Hospital Packages</Text>
+                       
+
+                   </View>
+                   <View style={{width:"50%", alignItems:"flex-end"}}>
+                        <View style={{
+                            backgroundColor:"#000",
+                            paddingHorizontal:20,
+                            paddingVertical:5,
+                            borderRadius:15
+                        }}>
+                            <Text style={{
+                                fontWeight:"bold",
+                                fontSize:13,
+                                color:"#fff"
+                            }}>View All</Text>
+                        </View>
+                   </View>
+               </View>
+
+                <ScrollView 
+                    horizontal
+                    showsHorizontalScrollIndicator={false}
+                    style={{height:400}}
+                >
+                    
+                    <TouchableOpacity 
+                        onPress={()=>navigation.navigate("Detail")}
+                        style={{
+                            height:250,
+                            elevation:2,
+                            backgroundColor:"#000",
+                            marginLeft:20,
+                            marginTop:20,
+                            borderRadius:15,
+                            marginBottom:10,
+                            width:160
+                        }}
+                    >
+                       
+                        <View style={{
+                            flexDirection:"row",
+                            paddingTop:10,
+                            paddingHorizontal:10,
+                            alignItems:"center"
+                        }}>
+                            <Text style={{
+                                fontWeight:"bold",
+                                fontSize: 25,
+                                color:"#fff"
+                            }}>The Rashtriya Kishor Swasthya Karyakram</Text>
+                           
+                        </View>
+                        <Text style={{
+                            paddingHorizontal:10,
+                            fontWeight:"bold",
+                            color:"#fff",
+                            paddingTop:35,
+                            alignSelf :'center'
+                        }}>
+                            APPLY NOW
+                        </Text>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity 
+                        onPress={()=>navigation.navigate("Detail")}
+                        style={{
+                            height:250,
+                            elevation:2,
+                            backgroundColor:"#FFF",
+                            marginLeft:20,
+                            marginTop:20,
+                            borderRadius:15,
+                            marginBottom:10,
+                            width:160
+                        }}
+                    >
+                       
+                        <View style={{
+                            flexDirection:"row",
+                            paddingTop:10,
+                            paddingHorizontal:10,
+                            alignItems:"center"
+                        }}>
+                            <Text style={{
+                                fontWeight:"bold",
+                                fontSize: 25
+                            }}>The Rashtriya Kishor Swasthya Karyakram</Text>
+                           
+                        </View>
+                        <Text style={{
+                            paddingHorizontal:10,
+                            fontWeight:"bold",
+                            color:"#b1e5d3",
+                            paddingTop:35,
+                            alignSelf :'center'
+                        }}>
+                            APPLY NOW
+                        </Text>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity 
+                        onPress={()=>navigation.navigate("Detail")}
+                        style={{
+                            height:250,
+                            elevation:2,
+                            backgroundColor:"#000",
+                            marginLeft:20,
+                            marginTop:20,
+                            borderRadius:15,
+                            marginBottom:10,
+                            width:160
+                        }}
+                    >
+                       
+                        <View style={{
+                            flexDirection:"row",
+                            paddingTop:10,
+                            paddingHorizontal:10,
+                            alignItems:"center"
+                        }}>
+                            <Text style={{
+                                fontWeight:"bold",
+                                fontSize: 25,
+                                color:"#fff"
+                            }}>The Rashtriya Kishor Swasthya Karyakram</Text>
+                           
+                        </View>
+                        <Text style={{
+                            paddingHorizontal:10,
+                            fontWeight:"bold",
+                            color:"#fff",
+                            paddingTop:35,
+                            alignSelf :'center'
+                        }}>
+                            APPLY NOW
+                        </Text>
+                    </TouchableOpacity>
+
+                </ScrollView>
       <Button
         title="Go to details screen"
         color= '#000000'
@@ -68,6 +381,7 @@ const HomeScreen = ({navigation}) => {
       />
 
       </View>
+      </ScrollView>
     );
 };
 
